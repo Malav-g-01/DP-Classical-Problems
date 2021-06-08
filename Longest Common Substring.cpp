4df6e48054640f194d7034b5fd2be20eb5af4fc9
Malav-g-01 <76492477+Malav-g-01@users.noreply.github.com>
2021-06-08 09:45:49 +0530

    Create Longest Common Substring.cpp

---
+++ Longest Common Substring.cpp
@@ -0,0 +1,92 @@
+/*
+    ENJOY CP !!!!
+    Author :- Malav Gajjar
+*/
+#include<bits/stdc++.h>
+//#include<ext/pd_ds/assoc_container.hpp>
+//#include<ext/pd_ds/tree_policy.hpp>
+//#include<ext/pd_ds/trie_policy.hpp>
+using namespace std;
+
+#define fio             ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+#define ll              long long int 
+#define ld              long double
+#define mod             1000000007
+#define inf             1e18
+#define endl            "\n"
+#define pb              push_back
+#define vi              vector<ll>
+#define vs              vector<string>
+#define pi              pair<ll, ll>
+#define ump             unordered_map
+#define mp              make_pair
+#define pq_max          priority_queue<ll>
+#define pq_min          priority_queue<ll, vi, greater<ll> >
+#define all(v)          v.begin(), v.end()
+#define ff              first
+#define ss              second
+#define rs              resize
+#define ub              upper_bound
+#define lb              lower_bound
+#define mid(l, r)       (l + (r-l)/2)
+#define bitc(x)         __builtin_popcount(x)
+#define sp(a)           setprecision(a)
+#define loop(a, b, c)   for(a=b; a<=c; a++)
+#define loop_r(a, b, c) for(a=b; a<=c; a++)
+#define itr(c)          for(auto it=c.begin(); it!=c.end(); it++)
+#define log(x)          cout << #x << ' ' << x;
+#define out(arr, a, b)  for(ll i=(a); i<=(b); i++)  cout << arr[i] << ' ';  cout << '\n';
+
+template <typename T> T gcd(T a, T b) { if(a%b) return gcd(b, a%b); return b; }
+template <typename T> T lcm(T a, T b) { return (a*b/gcd(a, b)); }
+
+void file_i_o()
+{
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+}
+
+ll LCS(string s, string t, ll n, ll m){
+    ll dp[n+1][m+1];
+    ll ans = 0;
+
+    for(ll i=0; i<=n; i++)
+        for(ll j=0; j<=m; j++)
+            if(i == 0 || j == 0)
+                dp[i][j] = 0;
+
+    for(ll i=1; i<=n; i++){
+        for(ll j=1; j<=m; j++){
+            if(s[i-1] == t[j-1]){
+                dp[i][j] = 1+dp[i-1][j-1];
+                ans = max(ans, dp[i][j]);
+            }
+
+            else
+                dp[i][j] = 0;
+        }
+    }
+
+    return ans;
+}
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    fio;
+    file_i_o();
+
+    ll t;
+    cin >> t;
+
+    while(t--){
+        string s, t;
+        cin >> s >> t;
+        ll n = s.length(), m = t.length();
+        cout << LCS(s, t, n, m) << '\n';
+    }
+    
+    return 0;
+}
